e91725b6ac6d239a8de8d0f4ca1d883e7b523ea6
jaesung <cjs6027@naver.com>
2019-04-30 22:44:06 +0900

    init Project

---
+++ ClassManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlueMarble
+{
+    class ClassManagement
+    {   
+        //모든 곳에서 같은 객체를 공유해야 할 경우에는 여기다가 객체를 만들어 넣고 사용하면 됩니다.
+        //객체를 불러올때에는 ClassManagement.GetInstance().~~~
+        private static ClassManagement s_instance;
+        private UserVO m_userVO; //여기다가 넣고 불러오면됨
+        private CityVO m_cityVO; 
+
+        public static ClassManagement GetInstance()
+        {
+            if (s_instance == null) s_instance = new ClassManagement();
+            return s_instance;
+        }
+    }
+}
